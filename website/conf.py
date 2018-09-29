6e3ee6b12035f60f2860bafe0cfe991bb6311c13
Markus Ullmann <mail@markus-ullmann.de>
2018-09-30 00:36:53 +0200

    Point base url to test site for now

--- website/conf.py
+++ website/conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Linux User Group Flensburg e.V."  # (translatable)
 BLOG_TITLE = "Linux User Group Flensburg e.V."  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://www.lugfl.de/"
+SITE_URL = "http://www2.lugfl.de/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://www.lugfl.de/"
