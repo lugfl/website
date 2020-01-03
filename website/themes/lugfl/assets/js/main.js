839788f91d5e8c3a8ac96f171fcaf5b8df9c89c8
Jan Boysen <jan@mediaweb-it.net>
2020-01-03 21:12:33 +0100

    little cleanup and more touch friendly jquery eventhandling

--- website/themes/lugfl/assets/js/main.js
+++ website/themes/lugfl/assets/js/main.js
@@ -7,25 +7,16 @@ $(document).ready(function(){
 	header_height 		 = $(".default-header").height(),
 	header_height_static = $(".site-header.static").outerHeight(),
 	fitscreen 			 = window_height - header_height;
-
-
 	$(".fullscreen").css("height", window_height)
 	$(".fitscreen").css("height", fitscreen);
-
-     
-     // -------   Active Mobile Menu-----//
-
-    $(".menu-bar").on('click', function(e){
-        e.preventDefault();
-        $(".navbar-nav").toggleClass('responsive');
-        $("span", this).toggleClass("lnr-menu lnr-cross");
-        $(".main-menu").addClass('mobile-menu');
-    });
-     
-    $('.img-pop-up').magnificPopup({
-        type: 'image',
-        gallery:{
-        enabled:true
-        }
-    });
- });
+	$(".menu-bar").on('click touch', function(e){
+		e.preventDefault();
+		$(".navbar-nav").toggleClass('responsive');
+		$("span", this).toggleClass("lnr-menu lnr-cross");
+		$(".main-menu").addClass('mobile-menu');
+	});
+	$('.img-pop-up').magnificPopup({
+		type: 'image',
+		gallery:{enabled:true}
+	});
+});
